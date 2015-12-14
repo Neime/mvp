337bb0d52e14c3d7d647da8a467cab991018796b
Neime <emilien.bouard@gmail.com>
2015-12-14 15:36:46 +0100

    User login

---
+++ src/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            return $this->redirectToRoute('homepage');
+        }
+        
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error'         => $authenticationUtils->getLastAuthenticationError(),
+        ));
+    }
+}
